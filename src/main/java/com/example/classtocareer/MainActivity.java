5d54972e8065ecda6d401c0eab176dac147f471d
Prl-1234 <priyal0917@gmail.com>
2020-12-07 19:41:08 +0530

    login activity layout created

--- src/main/java/com/example/classtocareer/MainActivity.java
+++ src/main/java/com/example/classtocareer/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.classtocareer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login=(Button) findViewById(R.id.loginbtn);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
